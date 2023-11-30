b5060a4f3b238a3caaa42caa51cfe4cca56edaa2
Meet Soni <64322534+MeetSoni@users.noreply.github.com>
2023-11-29 20:15:02 -0500

    CHANGED db

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 /*** Add to config/database.js ***/
 module.exports = {
-    url : "mongodb+srv://admin:admin%40123@cluster0.3cfsn76.mongodb.net/ASN4_Q2"
+    url : "mongodb+srv://meetsoni784:admin@cluster0.lrkxg.mongodb.net/ASN4_Q2"
 };
 
 
